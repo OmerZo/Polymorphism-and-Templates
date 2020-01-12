1740a90e3b7697413ac2a9a25e3fc221065ef1a6
zohky <zohky0528@gmail.com>
2020-01-12 15:25:23 +0200

    Start operator &&.

--- Interval.h
+++ Interval.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include "IComparable.h"
 #include "IPrintable.h"
+#include <string>
 
 using namespace std;
 
@@ -23,13 +24,22 @@ public:
 	virtual bool operator != (const IComparable<T>&) const;
 	virtual bool operator == (const IComparable<T>&) const;
 
+	string operator && (const Interval<T>&) const;
+	string operator || (const Interval<T>&) const;
+
+	bool isEmpty() const;
+	bool isBefore(const Interval<T>&) const;
+	bool isAfter(const Interval<T>&) const;
+	bool intersects(const Interval<T>&) const;
+
 	friend ostream& operator << (ostream& os, const Interval& inter)
 	{
-		cout << "ostream inline" << endl;
-		os << inter.x << ", " << inter.y << endl;
+		if (inter.x > inter.y)
+			os << "Invalid interval";
+		else
+			os << "(" << inter.x << ", " << inter.y << ")";
 		return os;
 	}
-
 
 	friend istream& operator >> (istream& in, Interval<T>& inter)
 	{
@@ -47,7 +57,6 @@ private:
 template <class T>
 Interval<T>::Interval(T x, T y)
 {
-	cout << x << ", " << y << endl;
 	this->x = x;
 	this->y = y;
 }
@@ -101,16 +110,60 @@ bool Interval<T>::operator==(const IComparable<T>& other) const
 	return false;
 }
 
-//template<class T>
-//ostream& operator << (ostream& os, const Interval<T>& interval)
-//{
-//	cout << "test1";
-//	//os << interval.x << ", " << interval.y << endl;
-//	return os;
-//}
-//
-//template <class T>
-//istream& operator >> (istream& in, Interval<T>& interval)
-//{
-//	return in;
-//}
+template<class T>
+string Interval<T>::operator&&(const Interval<T>& inter) const
+{
+	if(!this->intersects(inter))
+		return "EMPTY";
+
+	//if (this.contain(inter))
+		//return "(" + this->x + ", " + this->y + ")";
+
+	//if (inter.contain(this))
+		//return "(" + inter.x + ", " + inter.y + ")";
+
+	T x, y;
+	if ((this->x > inter.x) && (this->x < inter.y))
+	{
+		x = this->x;
+		y = inter.y;
+	}
+	else
+	{
+		x = inter.x;
+		y = this->y;
+	}
+	//return "(" + x + ", " + y + ")";
+	return x y;
+}
+
+template<class T>
+string Interval<T>::operator||(const Interval<T>& inter) const
+{
+	return "operator||";
+}
+
+
+template<class T>
+bool Interval<T>::isEmpty() const
+{
+	return (this->x == this->y);
+}
+
+template<class T>
+bool Interval<T>::isBefore(const Interval<T> &inter) const
+{
+	return (this->y <= inter.x);
+}
+
+template<class T>
+bool Interval<T>::isAfter(const Interval<T> &inter) const
+{
+	return (inter.y <= this->x);
+}
+
+template<class T>
+bool Interval<T>::intersects(const Interval<T>& inter) const
+{
+	return (((this->y > inter.x) && (this->y < inter.y)) || ((this->x > inter.x) && (this->x < inter.y)));
+}
--- Main.cpp
+++ Main.cpp
@@ -40,51 +40,51 @@ void testDateInput() {
 void testIntervalInt() {
 	Interval<int> interval = Interval<int>(2, 1);
 	cout << interval << endl;
-//	Interval<int> interval13(1, 3);
-//	Interval<int> interval24(2, 4);
-//	Interval<int> interval45(4, 5);
-//	cout << interval13 << endl;
-//	cout << interval24 << endl;
-//	cout << interval45 << endl;
+	Interval<int> interval13(1, 3);
+	Interval<int> interval24(2, 4);
+	Interval<int> interval45(4, 5);
+	cout << interval13 << endl;
+	cout << interval24 << endl;
+	cout << interval45 << endl;
 //	cout << "Does " << interval13 << " contain " << 2 << "? " << interval13.contains(2) << endl;
 //	cout << "Does " << interval24 << " contain " << 2 << "? " << interval24.contains(2) << endl;
 //	cout << "Does " << interval45 << " contain " << 2 << "? " << interval45.contains(2) << endl;
-//	cout << "Is " << interval13 << " before " << interval24 << "? "
-//		<< interval13.isBefore(interval24) << endl;
-//	cout << "Is " << interval13 << " before " << interval45 << "? "
-//		<< interval13.isBefore(interval45) << endl;
-//	cout << "Is " << interval24 << " before " << interval45 << "? "
-//		<< interval24.isBefore(interval45) << endl;
-//	cout << "Is " << interval13 << " after " << interval24 << "? "
-//		<< interval13.isAfter(interval24) << endl;
-//	cout << "Is " << interval13 << " after " << interval45 << "? "
-//		<< interval13.isAfter(interval45) << endl;
-//	cout << "Is " << interval24 << " after " << interval45 << "? "
-//		<< interval24.isAfter(interval45) << endl;
-//	cout << "Does " << interval13 << " intersect " << interval24 << "? "
-//		<< interval13.intersects(interval24) << endl;
-//	cout << "Does " << interval13 << " intersect " << interval45 << "? "
-//		<< interval13.intersects(interval45) << endl;
-//	cout << "Does " << interval24 << " intersect " << interval45 << "? "
-//		<< interval24.intersects(interval45) << endl;
-//	cout << "Does " << interval24 << " intersect " << interval13 << "? "
-//		<< interval24.intersects(interval13) << endl;
-//	cout << "Does " << interval45 << " intersect " << interval13 << "? "
-//		<< interval45.intersects(interval13) << endl;
-//	cout << "Does " << interval45 << " intersect " << interval24 << "? "
-//		<< interval45.intersects(interval24) << endl;
-//	cout << "interval13 && interval24 = " << (interval13 && interval24) << endl;
-//	cout << "interval13 && interval45 = " << (interval13 && interval45) << endl;
-//	cout << "interval24 && interval45 = " << (interval24 && interval45) << endl;
-//	cout << "interval24 && interval13 = " << (interval24 && interval13) << endl;
-//	cout << "interval45 && interval13 = " << (interval45 && interval13) << endl;
-//	cout << "interval45 && interval24 = " << (interval45 && interval24) << endl;
-//	cout << "interval13 || interval24 = " << (interval13 || interval24) << endl;
-//	cout << "interval24 || interval13 = " << (interval24 || interval13) << endl;
-//	cout << "interval13 || interval45 = " << (interval13 || interval45) << endl;
-//	cout << "interval24 || interval45 = " << (interval24 || interval45) << endl;
-//	cout << "interval45 || interval13 = " << (interval45 || interval13) << endl;
-//	cout << "interval45 || interval24 = " << (interval45 || interval24) << endl;
+	cout << "Is " << interval13 << " before " << interval24 << "? "
+		<< interval13.isBefore(interval24) << endl;
+	cout << "Is " << interval13 << " before " << interval45 << "? "
+		<< interval13.isBefore(interval45) << endl;
+	cout << "Is " << interval24 << " before " << interval45 << "? "
+		<< interval24.isBefore(interval45) << endl;
+	cout << "Is " << interval13 << " after " << interval24 << "? "
+		<< interval13.isAfter(interval24) << endl;
+	cout << "Is " << interval13 << " after " << interval45 << "? "
+		<< interval13.isAfter(interval45) << endl;
+	cout << "Is " << interval24 << " after " << interval45 << "? "
+		<< interval24.isAfter(interval45) << endl;
+	cout << "Does " << interval13 << " intersect " << interval24 << "? "
+		<< interval13.intersects(interval24) << endl;
+	cout << "Does " << interval13 << " intersect " << interval45 << "? "
+		<< interval13.intersects(interval45) << endl;
+	cout << "Does " << interval24 << " intersect " << interval45 << "? "
+		<< interval24.intersects(interval45) << endl;
+	cout << "Does " << interval24 << " intersect " << interval13 << "? "
+		<< interval24.intersects(interval13) << endl;
+	cout << "Does " << interval45 << " intersect " << interval13 << "? "
+		<< interval45.intersects(interval13) << endl;
+	cout << "Does " << interval45 << " intersect " << interval24 << "? "
+		<< interval45.intersects(interval24) << endl;
+	cout << "interval13 && interval24 = " << (interval13 && interval24) << endl;
+	cout << "interval13 && interval45 = " << (interval13 && interval45) << endl;
+	cout << "interval24 && interval45 = " << (interval24 && interval45) << endl;
+	cout << "interval24 && interval13 = " << (interval24 && interval13) << endl;
+	cout << "interval45 && interval13 = " << (interval45 && interval13) << endl;
+	cout << "interval45 && interval24 = " << (interval45 && interval24) << endl;
+	cout << "interval13 || interval24 = " << (interval13 || interval24) << endl;
+	cout << "interval24 || interval13 = " << (interval24 || interval13) << endl;
+	cout << "interval13 || interval45 = " << (interval13 || interval45) << endl;
+	cout << "interval24 || interval45 = " << (interval24 || interval45) << endl;
+	cout << "interval45 || interval13 = " << (interval45 || interval13) << endl;
+	cout << "interval45 || interval24 = " << (interval45 || interval24) << endl;
 }
 
 
